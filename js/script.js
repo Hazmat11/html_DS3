492328d55f1d0b0ec1d011d8cc995849301e576b
Hazmat <ebertrand@gaming.tech>
2021-11-13 16:55:16 +0100

    ajout d'une petite gallerie d'images

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 (document).ready(function(){
     (this).scrollTop(0);
 });
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
